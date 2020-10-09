e20f48820baa1d8e09ffc1f99cb273859734a151
Dave Borowski <dmborowski@msn.com>
2020-10-08 20:19:05 -0600

    Adding code to set scg params

--- d3_data_journalism/assets/js/app.js
+++ d3_data_journalism/assets/js/app.js
@@ -1,4 +1,20 @@
-// @TODO: YOUR CODE HERE!
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Define the chart's margins as an object
+var margin = {
+  top: 60,
+  right: 60,
+  bottom: 60,
+  left: 60
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+
 d3.csv("assets/data/data.csv").then(function(newsData) {
 
     console.log(newsData);
