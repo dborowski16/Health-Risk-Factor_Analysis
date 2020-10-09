f178224522a044c1dda4b74583fceccf35791d87
Dave Borowski <dmborowski@msn.com>
2020-10-08 18:40:43 -0600

    Adding code to bring in data from csv

--- d3_data_journalism/assets/js/app.js
+++ d3_data_journalism/assets/js/app.js
@@ -1,1 +1,17 @@
 // @TODO: YOUR CODE HERE!
+d3.csv("assets/data/data.csv").then(function(newsData) {
+
+    console.log(newsData);
+  
+    // log a list of states
+    var state = newsData.map(data => data.abbr);
+    console.log(state);
+  
+    // Cast each hours value in tvData as a number using the unary + operator
+    newsData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healtcare = +data.healtcare
+      console.log("Poverty:", data.poverty);
+      console.log("healthCare:", data.healthcare);
+    })
+});
